9dcf03dc1d90eff142cefbb60155f63ad2397870
Mohan Maha V <65824036+MohanMV@users.noreply.github.com>
2021-09-20 19:31:59 +0100

    Connected to Socket.IO from browser app

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 const { message } = require('statuses');
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(express.urlencoded({extended: false})); 
@@ -21,6 +23,10 @@ app.post('/messages', (req, res) => {
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user is connected')
+})
+
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
