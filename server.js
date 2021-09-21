d8ce8098db18c3c213d424c76d11cea5fdbac4c9
Mohan Maha V <65824036+MohanMV@users.noreply.github.com>
2021-09-21 19:55:40 +0100

    Set up Database

--- server.js
+++ server.js
@@ -3,29 +3,44 @@ const { message } = require('statuses');
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(express.urlencoded({extended: false})); 
 app.use(express.json());
 
-var messages = [
-    {name: 'Tim', message:'hii'},
-    {name: 'Jane', message:'Hello'}
+var dbUrl = 'mongodb+srv://user:user@learning-node.geipr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-]
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+    
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if(err)
+            sendStatus(500)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
 })
 
 io.on('connection', (socket) => {
     console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
