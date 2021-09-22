f7c48b21a161ff6c559fed46a7e49e7759bbce6f
Mohan Maha V <65824036+MohanMV@users.noreply.github.com>
2021-09-22 20:13:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,28 +27,33 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', async (req, res) => {
-    var message = new Message(req.body)
 
-    var savedMessage = await message.save()
-    
-    console.log('saved')
+    try {
+        var message = new Message(req.body)
 
-    var censored = await Message.findOne({message: 'badword'})
+        var savedMessage = await message.save()
+        
+        console.log('saved')
     
-    if(censored){
-        console.log('delete censored word')      
-        await Message.deleteOne({_id: censored.id})
+        var censored = await Message.findOne({message: 'badword'})
+        
+        if(censored)      
+            await Message.deleteOne({_id: censored.id})
+        else
+            io.emit('message', req.body)
+        
+        res.sendStatus(200)  
+        
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post calld')
     }
-    else
-        io.emit('message', req.body)
-    
-    res.sendStatus(200)
-    
-    // .catch((err) => {
-    //     res.sendStatus(500)
-    //     return console.error(err)
-    // })
+
 })
+
+ 
 
 io.on('connection', (socket) => {
     console.log('a user is connected')
