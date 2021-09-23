86755bdb74e375ae9aafe8a8ec8acb20d172ba32
Mohan Maha V <65824036+MohanMV@users.noreply.github.com>
2021-09-23 14:34:25 +0100

    Updates test spec

--- server.js
+++ server.js
@@ -26,31 +26,33 @@ app.get('/messages', (req, res) => {
     
 })
 
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+
+    Message.find({name: user}, (err, messages) => {
+        res.send(messages)
+    })
+    
+})
+
 app.post('/messages', async (req, res) => {
 
     try {
         var message = new Message(req.body)
-
-        var savedMessage = await message.save()
-        
-        console.log('saved')
-    
-        var censored = await Message.findOne({message: 'badword'})
-        
+        var savedMessage = await message.save()        
+        console.log('saved')   
+        var censored = await Message.findOne({message: 'badword'})      
         if(censored) 
             await Message.deleteOne({_id: censored.id})
         else
-            io.emit('message', req.body)
-        
-        res.sendStatus(200)  
-        
+            io.emit('message', req.body)      
+        res.sendStatus(200)        
     } catch (error) {
         res.sendStatus(500)
         return console.error(error)
     } finally {
-        console.log('message post calld')
+        console.log('message post called')
     }
-
 })
 
  
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -10,6 +10,32 @@ describe('get message', () =>{
     it('should return 200 ok', (done) => {
         request.get('http://localhost:3000/messages', (err, res) => {
             console.log(res.body)
+            expect(res.statusCode).toEqual(200)
+            done()
+        })
+    })
+
+    it('should return list that is not empty', (done) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
+            console.log(res.body)
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0)
+            done()
+        })
+    })
+})
+
+describe('get message from specific user', () =>{
+    it('should return 200 ok', (done) => {
+        request.get('http://localhost:3000/messages/tim', (err, res) => {
+            console.log(res.body)
+            expect(res.statusCode).toEqual(200)
+            done()
+        })
+    })
+    it('name should be tim', (done) => {
+        request.get('http://localhost:3000/messages/tim', (err, res) => {
+            console.log(res.body)
+            expect(JSON.parse(res.body)[0].name).toEqual('tim')
             done()
         })
     })
